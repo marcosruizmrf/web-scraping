8a5362903c66788092e5cf88fb055cd384954e3e
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2022-04-06 23:26:52 -0300

    style: Editing text and removing duplicate imports

--- requisicoes.py
+++ requisicoes.py
@@ -3,14 +3,13 @@ from wsgiref import headers
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 options = Options()
-options.add_argument('--headless') #não mostrar o navegador
+options.add_argument('--headless') # não mostrar o navegador
 options.add_argument('window-size=400,800')
 options.add_experimental_option("detach", True)
 
@@ -20,7 +19,7 @@ navegador.get('https://marcosruizmrf.github.io/front-pgf/')
 sleep(2)
 
 button_stay = navegador.find_element_by_css_selector('button > span')
-#como clicar no segundo botao do site?
+#TODO: clicar no segundo botao
 #nextButton = navegador.find_elements_by_tag_name('button')[-4]
 button_stay.click()
 
