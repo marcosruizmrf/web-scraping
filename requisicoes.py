ea8e11d34da67a2688ae3486a5b67744b1ba7da6
Marcos Ruiz <marcosruizmrf@outlook.com>
2022-04-26 22:20:04 -0300

    feat: Data export to excel using DataFrame

--- requisicoes.py
+++ requisicoes.py
@@ -1,15 +1,13 @@
-from wsgiref import headers
 
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+import pandas as pd
 
 options = Options()
-options.add_argument('--headless') # não mostrar o navegador
+#options.add_argument('--headless')  # não mostrar o navegador
 options.add_argument('window-size=400,800')
 options.add_experimental_option("detach", True)
 
@@ -19,25 +17,63 @@ navegador.get('https://marcosruizmrf.github.io/front-pgf/')
 sleep(2)
 
 button_stay = navegador.find_element_by_css_selector('button > span')
-#TODO: clicar no segundo botao
-#nextButton = navegador.find_elements_by_tag_name('button')[-4]
+# TODO: clicar no segundo botao
+# nextButton = navegador.find_elements_by_tag_name('button')[-4]
 button_stay.click()
 
-sleep(4)
+sleep(2)
 
 page_content = navegador.page_source
 site = BeautifulSoup(page_content, 'html.parser')
 print(site.prettify())
 sleep(2)
+'''
 noticia = site.findAll("mat-card-header")
 for header in noticia:
-   header.get('class'), print(header.find('mat-card-title').text[0:20])
+   header.get('class'), print(header.find('mat-card-title').text[0:20])'''
+dados_contents = []
+
+contents = site.findAll('div', attrs={'class': 'content'})
+for content in contents:
+    title = site.findAll('mat-card-title', attrs={'class': 'mat-card-title'})[-1].text
+    print('Título:', title)
+
+    description = site.findAll('mat-card-subtitle', attrs={'class': 'mat-card-subtitle'})[0].text
+    print('Descrição:', description)
+
+    orderOfService = site.findAll('mat-card-subtitle', attrs={'class': 'mat-card-subtitle'})[-1].text
+    print('Ordem de serviço:', orderOfService)
+
+    print()
+
+    dados_contents.append([title, description, orderOfService])
+
+
+dados = pd.DataFrame(dados_contents, columns=[{'Titulo'}, {'Descricao'}, {'Ordem de Servico'}])
+
+dados.to_csv('contents2.csv', index=False, encoding='utf-8-sig')
+
+
+
+
+'''title = [detalhe.text for detalhe in title]
+print(title)'''
+
+'''titulo = site.findAll("mat-card-header")
+for header in titulo:
+   header.get('class'), print(header.find('mat-card-subtitle').text)'''
+
+'''divoContent = site.findAll("div")
+for content in divoContent:
+   content.get('class'), print(content.find('mat-card').text)'''
+
+'''container = site.findAll('div', attrs={'class': 'content'})
+container = [detalhe.text for detalhe in container]
+container.append(container.count(container))
+print(container)'''
 
 '''
 input_place = navegador.find_element_by_tag_name('input')
 input_place.send_keys('São Paulo')
 input_place.submit()
 '''
-
-
-
